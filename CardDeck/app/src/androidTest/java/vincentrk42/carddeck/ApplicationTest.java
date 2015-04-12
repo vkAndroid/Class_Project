3c0d99c90d661a20386ee3157e83a3ed26e30d9b
unknown <vincent@koeten.net>
2015-04-12 14:13:51 -0700

    Initial commit of empty project with blank activity.

---
+++ CardDeck/app/src/androidTest/java/vincentrk42/carddeck/ApplicationTest.java
@@ -0,0 +1,13 @@
+package vincentrk42.carddeck;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
